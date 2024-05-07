c745b18e31c9c510fc0f4f801b5534f711b76b8d
Fabricio Dias <fabricio270909@gmail.com>
2024-05-06 22:37:24 -0300

    Exercise 1: write if a number is negative or not

---
+++ ConditionalStructure/Exerc1.cs
@@ -0,0 +1,18 @@
+﻿namespace ConditionalStructure;
+
+public class Exerc1
+{
+    public void readNumber()
+    {
+        Console.Write("Write some number: ");
+        int numberToRead = int.Parse(Console.ReadLine());
+
+        if (numberToRead >= 0)
+        {
+            Console.WriteLine("Not negative");
+        }else
+        {
+            Console.WriteLine("Negative");
+        }
+    }
+}
--- ConditionalStructure/Program.cs
+++ ConditionalStructure/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
-namespace ConditionalStructure // Note: actual namespace depends on the project name.
+namespace ConditionalStructure
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string? option;
+
+            do
+            {
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("1 - Task 1: write if a number is negative");
+                
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("Select an option: ");
+                
+                option = Console.ReadLine();
+                Console.Clear();
+                switch (option)
+                {
+                    case "1":
+                        var exercise1 = new Exerc1();
+                        exercise1.readNumber();
+                        break;
+                    case "2":
+                        
+                        break;
+                    case "3":
+                        
+                        break;
+                    
+                    case "0":
+                        break;
+                }
+                
+                Console.WriteLine("----------------------------------");
+            } while (option != "0");
+            
         }
+        
     }
 }
